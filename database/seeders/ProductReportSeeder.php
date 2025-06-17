be1cbaa18c1ee67fcb495152146362db5ae6299d
Drashti Darji <drashti.darji@iqinfinite.in>
2025-06-17 14:35:13 +0530

    use faker and generate data between one month

--- database/seeders/ProductReportSeeder.php
+++ database/seeders/ProductReportSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\ProductReport;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
 
 class ProductReportSeeder extends Seeder
 {
@@ -13,6 +15,17 @@ class ProductReportSeeder extends Seeder
      */
     public function run(): void
     {
-       ProductReport::factory(5)->create();
+        //ProductReport::factory(5)->create();
+        $faker = Faker::create();
+
+        foreach (range(1, 10) as $index) {
+            DB::table('product_reports')->insert([
+                'p_id' => rand(1, 10),
+                'report_date' => $faker->dateTimeBetween('2025-06-01', 'now'),
+                'remaining_qty' => rand(1, 10),
+                'created_at' => now(),
+                'updated_at' => now()
+            ]);
+        }
     }
 }
