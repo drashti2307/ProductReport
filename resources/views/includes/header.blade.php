a59ae2fdeddb9e9c24762bbed224a70e51135e93
Naresh Vanzara <naresh.vanzara@iqinfinite.in>
2025-06-19 12:14:17 +0530

    Added logo image for report in header

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,7 +1,15 @@
-<div style="padding:10px;position:relative;">
-    <h2>Product Report</h2>
+<div style="padding:10px 10px 0;position:relative;">
+
+    <div style="position:absolute;left:0;top:12px;">
+        <img src="{{ public_path('image/image.png') }}" style="width: 130px;height:100px">
+    </div>
+
+    <h2>Product Report </Br>
+        <p>Weekly Report</p>
+
+    </h2>
     <div style="position:absolute;right:0;top:72px;">
-        Created: @php echo date('d-m-y') @endphp
+        Created: @php echo date('d-m-Y') @endphp
     </div>
 </div>
 <hr style=" border: none;
